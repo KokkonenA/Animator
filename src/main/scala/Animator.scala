951cb6068e74711af9af826a10d1d5fa35f8f1af
Antti Kokkonen <akn@saunalahti.fi>
2021-03-08 13:47:58 +0200

    File Animator.scala created. Basic layout of the app sketched

---
+++ src/main/scala/Animator.scala
@@ -0,0 +1,49 @@
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.layout.{Background, BackgroundFill, ColumnConstraints, CornerRadii, GridPane, HBox, RowConstraints, VBox}
+import scalafx.scene.paint.Color.{Blue, Gray}
+import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.scene.canvas.Canvas
+
+object Animator extends JFXApp {
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Stick Figure Animator"
+    width = 1200
+    height = 675
+  }
+
+  val root = new GridPane
+  val scene = new Scene(root)
+  stage.scene = scene
+
+  val leftMenu = new VBox
+  val rightMenu = new VBox
+  val timeline = new HBox
+  val viewer = new Canvas(720, 480)
+
+  root.add(leftMenu, 0, 0)
+  root.add(rightMenu, 2, 0)
+  root.add(timeline, 1, 0)
+  root.add(viewer, 0, 1)
+
+  //Define grid row and column size
+  val column0 = new ColumnConstraints
+  val column1 = new ColumnConstraints
+  val column2 = new ColumnConstraints
+  val row0 = new RowConstraints
+  val row1 = new RowConstraints
+
+  column0.percentWidth = 15
+  column1.percentWidth = 70
+  column2.percentWidth = 15
+  row0.percentHeight = 84
+  row1.percentHeight = 16
+
+  root.columnConstraints = Array[ColumnConstraints](column0, column1, column2)
+  root.rowConstraints = Array[RowConstraints](row0, row1)
+
+  leftMenu.background = new Background(Array(new BackgroundFill((Gray), CornerRadii.Empty, Insets.Empty)))
+  rightMenu.background = new Background(Array(new BackgroundFill((Gray), CornerRadii.Empty, Insets.Empty)))
+  timeline.background = new Background(Array(new BackgroundFill((Blue), CornerRadii.Empty, Insets.Empty)))
+}
